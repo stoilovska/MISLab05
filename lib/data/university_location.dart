3e7cbeadea44cf59351b47e7ab336ee3a9730942
stoilovska <teodora.stoilovska@gmail.com>
2024-02-19 22:51:02 +0100

    add comment

--- lib/data/university_location.dart
+++ lib/data/university_location.dart
@@ -1,4 +1,6 @@
 import 'package:mis_lab_03_201076/models/Location.dart';
 
+// University of Oxford
+
 Location university =
     Location("University of Oxford", 51.75532695059838, -1.2543384170457834);
